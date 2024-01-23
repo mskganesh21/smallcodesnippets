8abf7ac0b5430ade233e127b054e48f5a23706db
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-01-24 00:36:51 +0530

    Create convertimagetobase64.js
    converts small images to url's so that we can directly load them instead of storing it in a 3rd party database

---
+++ utils/convertimagetobase64.js
@@ -0,0 +1,18 @@
+//this function helps small sized images to be loaded as url's so that we don't need any cloudinary like libraries to do so 
+
+
+/** image onto base64 */
+export default function convertToBase64(file){
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+            resolve(fileReader.result)
+        }
+
+        fileReader.onerror = (error) => {
+            reject(error)
+        }
+    })
+}
