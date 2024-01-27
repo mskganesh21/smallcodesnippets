1c6633711b2ee4e354e39ecbdb748438e92a7c58
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-01-27 14:42:50 +0530

    Update SampleModel.js

--- db/SampleModel.js
+++ db/SampleModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 
 // we are creating a new mongodb schema which is the document structure
 const userSchema = new mongoose.Schema(
@@ -22,5 +22,5 @@ const userSchema = new mongoose.Schema(
 
 // we are creating a model which means we are creating a collection of Users.
 // having a singular and lowercase model name is convenient and it follows convention
-const User = mongoose.model('user', userSchema)
-module.exports = User
+const User = new mongoose.model('user', userSchema)
+export default User;
