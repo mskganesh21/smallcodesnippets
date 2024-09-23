fa756bed141b741a712d43b633a1223e6c707a8f
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2024-09-23 13:01:26 +0530

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,53 @@
+/*
+
+sitemap in react
+
+3 ways
+
+1.for static content applications
+1.1use sitemap npm library
+
+this is done in build process
+generate a script to do 
+using sitemaptoStream,streamToPromise
+
+this creates a public folder during build
+and then creates a sitemap.xml file
+
+1.2 using react-router-sitemap
+it is easier to do so 
+Sitemap func from react-router-sitemap
+.build and .save
+
+
+2. using nextjs and npm sitemap
+similar to procedure 1
+
+
+
+coming to e-commerce website
+
+1.use sitemap npm if products don't change
+2.if nextjs then the approach is api based
+and on each request we generate sitemap
+*/
+
+
+
+/*
+generate dynamic sitemap with a 
+headless cms
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
